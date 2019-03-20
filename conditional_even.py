45d71f67d8eda6c2f6766b12964a2a6a90e53633
Gagan <gaganshetty94@gmail.com>
2019-03-20 19:44:13 +0530

    Initial commit

---
+++ conditional_even.py
@@ -0,0 +1,23 @@
+count = {}
+
+count[(1,2,4)]=5
+print(count)
+count[(4,2,1)]=7
+print(count)
+count[(1,2)]=6
+print(count)
+count[(4,2,1)]=2
+print(count)
+# tot = 0
+# print(count)
+# for i in count:
+# 	print(count)
+# 	tot = tot + count[i]
+# 	print(count)
+# print(len(count)+tot) 
+
+
+count={'(1,2,4)':5,
+	   '(4,2,1)':7,
+	   '(1,2)':6,
+	   '(4,2,1)':2}
