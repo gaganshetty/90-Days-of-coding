ce6f577925e0dc77582685e55be9c1f138704c20
Gagan <gaganshetty94@gmail.com>
2019-03-22 23:47:48 +0530

    Password Validation Algorithms

---
+++ password_validate.py
@@ -0,0 +1,28 @@
+import time
+
+def error():
+	print("Invalid password pattern")
+
+
+s = input("Enter the password: \n")
+start = time.time()
+if len(s)< 8:
+	error()
+elif  not any(c.isalnum()  for c in s):
+	error();
+elif  not any(c.isalpha() for c in s):
+	error();
+elif  not any(c.isdigit() for c in s):
+	error();
+elif  not any(c.islower() for c in s):
+	error();
+elif  not any(c.isupper() for c in s):
+	error();
+else:
+	print('Success')
+
+time.sleep(1)
+end = time.time()
+total = end-start
+
+print("Total", total, "s" )
---
+++ password_verify.py
@@ -0,0 +1,10 @@
+import re
+password = input("Enter the password\n")
+
+def error():
+	print("password doesnot match validaton pattern")
+
+if (len(password)>8) and (re.search("[a-z]",password)) and (re.search("[A-Z]",password)) and (re.search("[0-9]",password)):
+	print("Success")
+else:
+	error()
